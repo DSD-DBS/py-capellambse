ed96e1cf34d265239aa002ccb34d10757d230c92
vik378 <electron378@gmail.com>
2021-11-20 17:51:30 +0100

    added sphynx ext for x-referencing

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -56,7 +56,7 @@ extensions = [
     "sphinx_rtd_theme",
     "jinja_in_rst",
     "capellambse.sphinx",
-    'sphinx.ext.autosectionlabel',
+    "sphinx.ext.autosectionlabel",
 ]
 
 # Add any paths that contain templates here, relative to this directory.
