49d66cb500a561df3bab6b3133a14efc48439f2d
vik378 <electron378@gmail.com>
2022-01-14 22:00:53 +0100

    Implements missing  link for #48
    * added link between port and state machines (fa)
    * updated API model
    * introduced test for new functionality

---
+++ tests/test_xlayer_fa.py
@@ -0,0 +1,13 @@
+# pylint: disable=no-self-use
+import typing as t
+
+import pytest
+
+import capellambse.model.common as c
+from capellambse import MelodyModel
+
+
+def test_function_port_has_state_machines(model: MelodyModel):
+    port = model.by_uuid("226878bc-3e4b-4236-ba72-996fc1d988c0")
+    stm = model.by_uuid("06cefb2b-534e-4453-9aba-fe53329197ad")
+    assert stm in port.state_machines
