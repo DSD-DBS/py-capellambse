65230bfa00a1f8c6b6a5c37a097213d540a7642a
Ernst Würger <ernst.wuerger@deutschebahn.com>
2022-01-19 11:24:07 +0100

    Moved pa tests to the test_model_layers tests

--- tests/test_model_layers.py
+++ tests/test_model_layers.py
@@ -566,9 +566,10 @@ class TestArchitectureLayers:
                     "all_actors",
                     "all_components",
                     # TODO: actor_exchanges
-                    # TODO: component_exchanges
+                    "all_component_exchanges",
                     "all_physical_exchanges",
                     "all_physical_links",
+                    "all_physical_paths",
                 ],
                 id="PhysicalArchitectureLayer",
             ),
@@ -694,3 +695,13 @@ class TestArchitectureLayers:
 
         assert card1_os in comp_card1.deployed_components
         assert comp_card1 in card1_os.deploying_components
+
+    def test_physical_path_is_found(self, model: MelodyModel) -> None:
+        expected_path = model.by_uuid("42c5ffb3-29b3-4580-a061-8f76833a3d37")
+        assert expected_path in model.pa.all_physical_paths
+
+    def test_pa_component_exchange_is_found(self, model: MelodyModel) -> None:
+        expected_exchange = model.by_uuid(
+            "3aa006b1-f954-4e8f-a4e9-2e9cd38555de"
+        )
+        assert expected_exchange in model.pa.all_component_exchanges
