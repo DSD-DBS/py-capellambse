b5ad99602e3e7736d3a9db9e7eeecce7b13fe83d
ewuerger <ernst.wuerger@gmail.com>
2022-05-25 16:17:10 +0200

    Remove unused imports

--- capellambse/model/crosslayer/__init__.py
+++ capellambse/model/crosslayer/__init__.py
@@ -6,7 +6,7 @@
 .. diagram:: [CDB] BaseLayer
 """
 from .. import common as c
-from . import capellacommon, capellacore, cs, fa, information, interaction
+from . import cs, information
 
 
 class BaseArchitectureLayer(c.GenericElement):
