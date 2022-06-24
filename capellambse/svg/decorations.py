8f29e779e7090ae7b91de99ed8e4b12c2a2aec56
Martin Lehmann <martin.lehmann@deutschebahn.com>
2022-06-24 15:44:03 +0200

    fix(svg): Fix missing space in docstring

--- capellambse/svg/decorations.py
+++ capellambse/svg/decorations.py
@@ -14,7 +14,7 @@ logger = logging.getLogger(__name__)
 icon_size = 20
 """Default icon size."""
 icon_padding = 2
-"""Default icon padding(left/right side)."""
+"""Default icon padding (left/right side)."""
 feature_space = 24
 """Default margins/padding (top/bot and left/right) for feature text."""
 
