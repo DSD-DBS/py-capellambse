58e5805c990ab3e900102d8662c1e0b9c44cdf78
Martin Lehmann <martin.lehmann@deutschebahn.com>
2022-09-22 15:28:51 +0200

    test: Improve `cs` tests
    Instead of only looking at the first two elements, compare the entire
    lists' elements' UUIDs against what is expected.

--- tests/test_xlayer_cs.py
+++ tests/test_xlayer_cs.py
@@ -5,19 +5,30 @@ from capellambse import MelodyModel
 
 
 def test_physical_path_has_ordered_list_of_involved_items(model: MelodyModel):
+    expected = [
+        "544549d6-2aa4-44c2-b2ae-a86302f48e62",
+        "42ee9e89-d445-45a2-8280-028d4fb1038d",
+        "ea8cd402-3d9a-469a-a2ce-2bc1252c3a01",
+        "3078ec08-956a-4c61-87ed-0143d1d66715",
+        "baa3047c-9cb4-40a7-9b67-b9b5f76fd2ee",
+    ]
+
     path = model.pa.all_physical_paths.by_name("card1 - card2 connection")
-    cmp_0_part = model.search("Part").by_name("Compute Card 1")
-    link_1 = model.pa.all_physical_links.by_name("Eth Cable 2")
-    assert cmp_0_part == path.involved_items[0]
-    assert link_1 == path.involved_items[1]
+
+    actual = [i.uuid for i in path.involved_items]
+    assert actual == expected
 
 
 def test_physical_path_has_ordered_list_of_involved_links(model: MelodyModel):
+    expected = [
+        "42ee9e89-d445-45a2-8280-028d4fb1038d",
+        "3078ec08-956a-4c61-87ed-0143d1d66715",
+    ]
+
     path = model.pa.all_physical_paths.by_name("card1 - card2 connection")
-    link_0 = model.pa.all_physical_links.by_name("Eth Cable 2")
-    link_1 = model.pa.all_physical_links.by_name("Eth Cable 3")
-    assert link_0 == path.involved_links[0]
-    assert link_1 == path.involved_links[1]
+
+    actual = [i.uuid for i in path.involved_links]
+    assert actual == expected
 
 
 def test_physical_path_has_exchanges(model: MelodyModel):
