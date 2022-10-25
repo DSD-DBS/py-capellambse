1c4997ef906af2e9cc5903bd118b7f45daa6c0a5
ewuerger <ernst.wuerger@gmail.com>
2022-10-25 14:30:01 +0200

    test(model): Fix naming of `cs` test cases

--- tests/test_xlayer_cs.py
+++ tests/test_xlayer_cs.py
@@ -4,7 +4,7 @@
 from capellambse import MelodyModel
 
 
-def test_physical_path_has_ordered_list_of_involved_items(model: MelodyModel):
+def test_PhysicalPath_has_ordered_list_of_involved_items(model: MelodyModel):
     expected = [
         "544549d6-2aa4-44c2-b2ae-a86302f48e62",
         "42ee9e89-d445-45a2-8280-028d4fb1038d",
@@ -19,7 +19,7 @@ def test_physical_path_has_ordered_list_of_involved_items(model: MelodyModel):
     assert actual == expected
 
 
-def test_physical_path_has_ordered_list_of_involved_links(model: MelodyModel):
+def test_PhysicalPath_has_ordered_list_of_involved_links(model: MelodyModel):
     expected = [
         "42ee9e89-d445-45a2-8280-028d4fb1038d",
         "3078ec08-956a-4c61-87ed-0143d1d66715",
@@ -31,19 +31,19 @@ def test_physical_path_has_ordered_list_of_involved_links(model: MelodyModel):
     assert actual == expected
 
 
-def test_physical_path_has_exchanges(model: MelodyModel):
+def test_PhysicalPath_has_exchanges(model: MelodyModel):
     exchange = model.pa.all_component_exchanges.by_name("C 6")
     path = model.pa.all_physical_paths.by_name("card1 - card2 connection")
     assert path.exchanges == [exchange]
 
 
-def test_physical_link_has_physical_paths(model: MelodyModel):
+def test_PhysicalLink_has_physical_paths(model: MelodyModel):
     link = model.pa.all_physical_links.by_name("Eth Cable 2")
     path = model.pa.all_physical_paths.by_name("card1 - card2 connection")
     assert link.physical_paths == [path]
 
 
-def test_physical_link_has_exchanges(model: MelodyModel):
+def test_PhysicalLink_has_exchanges(model: MelodyModel):
     link = model.pa.all_physical_links.by_name("Eth Cable 2")
     exchange = model.pa.all_component_exchanges.by_name("C 3")
     assert link.exchanges == [exchange]
