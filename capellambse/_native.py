6de29dc14e3f7440a197ecdd587e795c62aa9881
Paula-Kli <Paula-Kli@users.noreply.github.com>
2023-07-12 10:51:17 +0200

    feat(native): Copy files into / out of docker container
    The Docker runner for capellambse's Native Capella functions used to use
    volume mounts for making the project and workspace available to the
    container. While it's the simplest approach to implement, it has a few
    drawbacks. The most notable one is that it doesn't work properly if
    capellambse itself runs within a container and was given permission to
    use Docker-in-Docker with the host's docker engine. As CI (capellambse's
    primary target environment) is usually based around docker containers or
    something similar, this drawback has become unacceptable.
    
    This commit therefore changes the behavior of the Docker runner to copy
    the project and workspace files into the spawned "slave" container
    before executing Capella inside of it, and to copy the modified files
    back to their original locations afterwards.

--- capellambse/_native.py
+++ capellambse/_native.py
@@ -9,9 +9,12 @@ import collections.abc as cabc
 import contextlib
 import dataclasses
 import errno
+import json
 import logging
 import pathlib
+import random
 import shutil
+import string
 import subprocess
 import tempfile
 import typing as t
@@ -137,6 +140,7 @@ class _SimpleRunner:
 class _DockerRunner:
     def __init__(self, image: str) -> None:
         self.image = image
+        self.entrypoint = self.get_entrypoint()
 
     def __call__(
         self,
@@ -145,27 +149,115 @@ class _DockerRunner:
         /,
         *args: str | pathlib.Path,
     ) -> subprocess.CompletedProcess:
-        cmd: list[str | pathlib.Path] = [
-            "docker",
-            "run",
-            "--rm",
-            "--detach=false",
-            f"-v{workspace}:/workspace",
-            f"-v{project}:/model",
-            self.image,
-            *_COMMON_ARGS,
-            _ARG_WORKSPACE,
-            "/workspace",
-            _ARG_PROJECT,
-            "/model/main_model",
-            *args,
-        ]
-        _LOGGER.debug("Running Capella in Docker with command %r", cmd)
-        return subprocess.run(
-            cmd,
-            stdin=subprocess.DEVNULL,
-            stdout=subprocess.PIPE,
-            stderr=subprocess.STDOUT,
+        container_name = "capellambse-" + "".join(
+            random.choice(string.ascii_lowercase) for i in range(5)
+        )
+        with self.start_container(container_name):
+            self.copy_files_to_container(
+                workspace, "/workspace", container_name
+            )
+            self.copy_files_to_container(project, "/model", container_name)
+
+            capella = self.run_capella_in_container(
+                container_name, self.entrypoint, *args
+            )
+
+            self.copy_files_from_container(
+                "/workspace/.", workspace, container_name
+            )
+            self.copy_files_from_container("/model/.", project, container_name)
+
+        return capella
+
+    def get_entrypoint(self) -> list[str]:
+        subprocess.run(["docker", "pull", self.image], check=True)
+        proc = subprocess.run(
+            ["docker", "inspect", self.image],
             check=True,
-            text=True,
+            stdout=subprocess.PIPE,
+            encoding="utf-8",
+        )
+        parsed_output = json.loads(proc.stdout)[0]
+        ep = parsed_output["Config"]["Entrypoint"]
+        if not ep:
+            raise ValueError(f"Docker image has no entrypoint: {self.image}")
+        return ep
+
+    @contextlib.contextmanager
+    def start_container(self, name: str) -> cabc.Iterator[None]:
+        _LOGGER.debug("Starting container %s from %s", name, self.image)
+        with subprocess.Popen(
+            [
+                "docker",
+                "run",
+                "--rm",
+                "-i",
+                "--entrypoint",
+                "/bin/sh",
+                "--name",
+                name,
+                self.image,
+                "-ec",
+                "echo OK; read REPLY || true",
+            ],
+            stdin=subprocess.PIPE,
+            stdout=subprocess.PIPE,
+        ) as process:
+            assert process.stdin is not None is not process.stdout
+            try:
+                if process.stdout.readline() != b"OK\n":
+                    raise RuntimeError(
+                        f"Failed handshake with container image {self.image}"
+                    )
+
+                _LOGGER.debug("Started container %s", name)
+                yield
+            finally:
+                _LOGGER.debug("Stopping container %s", name)
+                process.communicate(b"\n", timeout=30)
+
+    def run_capella_in_container(
+        self,
+        container_name: str,
+        entrypoint: cabc.Sequence[str],
+        *args: str | pathlib.Path,
+    ) -> subprocess.CompletedProcess:
+        return subprocess.run(
+            [
+                "docker",
+                "exec",
+                f"{container_name}",
+                *entrypoint,
+                *_COMMON_ARGS,
+                _ARG_WORKSPACE,
+                "/workspace",
+                _ARG_PROJECT,
+                "/model/main_model",
+                *args,
+            ],
+            check=True,
+        )
+
+    def copy_files_to_container(
+        self, directory: pathlib.Path, destination: str, container_name: str
+    ) -> None:
+        subprocess.check_call(
+            [
+                "docker",
+                "cp",
+                f"{directory}",
+                f"{container_name}:{destination}",
+            ],
+        )
+
+    def copy_files_from_container(
+        self, directory: str, destination: pathlib.Path, container_name: str
+    ) -> None:
+        subprocess.check_call(
+            [
+                "docker",
+                "cp",
+                f"{container_name}:{directory}",
+                f"{destination}",
+            ],
         )
