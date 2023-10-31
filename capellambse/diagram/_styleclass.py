7e2cab6077f3eb6a11a743657673e3f3768c84a3
ewuerger <ernst.wuerger@gmail.com>
2023-10-31 15:27:53 +0100

    feat: Add missing attributes and default styling

--- capellambse/diagram/_styleclass.py
+++ capellambse/diagram/_styleclass.py
@@ -30,8 +30,8 @@ def get_styleclass(obj: model.ModelObject) -> str:
 
 
 def _default(obj: model.ModelObject) -> str:
-    if isinstance(obj, model.GenericElement):
-        return obj.xtype.split(":")[-1]
+    if type(obj).__name__ == "GenericElement":
+        return obj.xtype.split(":")[-1]  # type: ignore[attr-defined]
     return type(obj).__name__
 
 
