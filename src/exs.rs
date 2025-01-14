cb28917eb8e26499d02ebd0ce4b7fa42700b5aca
Martin Lehmann <martin.lehmann@deutschebahn.com>
2025-01-14 09:57:55 +0100

    perf(exs): Write chunks during serialization
    Write out chunks of up to 2MB to the target file while serializing a
    tree. This drastically reduces peak memory usage with large models.

--- src/exs.rs
+++ src/exs.rs
@@ -15,7 +15,7 @@ use pyo3::{
     types::{PyDict, PyString, PyType},
 };
 
-const DEFAULT_BUFFER_SIZE: usize = 2 * 1024 * 1024; // 2 MiB
+const MEM_BUFFER_SIZE: usize = 2 * 1024 * 1024; // 2 MiB
 
 #[cfg(not(windows))]
 const LINESEP: &[u8; 1] = b"\n";
@@ -36,59 +36,69 @@ static EARLY_NAMESPACES: LazyLock<HashSet<&'static str>> = LazyLock::new(|| {
 });
 
 #[pyfunction]
-#[pyo3(signature=(tree, /, *, line_length, siblings))]
+#[pyo3(signature=(tree, /, *, line_length, siblings, file))]
 pub fn serialize<'py>(
     py: Python<'py>,
     tree: &'py Bound<PyAny>,
     line_length: usize,
     siblings: bool,
-) -> PyResult<Vec<u8>> {
-    Ok(Serializer::new(py, line_length)
+    file: Option<Bound<PyAny>>,
+) -> PyResult<Option<Vec<u8>>> {
+    Ok(Serializer::new(py, line_length, file)?
         .feed_tree(tree, siblings)?
-        .finish())
+        .finish()?)
 }
 
-struct Serializer {
+struct Serializer<'py> {
     buf: Vec<u8>,
     pos: usize,
     line_length: usize,
+    write: Option<Bound<'py, PyAny>>,
 
-    etree_element: Py<PyType>,
-    etree_comment: Py<PyType>,
+    etree_element: Bound<'py, PyType>,
+    etree_comment: Bound<'py, PyType>,
 }
 
-impl Serializer {
-    fn new(py: Python<'_>, line_length: usize) -> Self {
+impl<'py> Serializer<'py> {
+    fn new(
+        py: Python<'py>,
+        line_length: usize,
+        output: Option<Bound<'py, PyAny>>,
+    ) -> PyResult<Self> {
         let etree = py.import("lxml.etree").expect("cannot import lxml.etree");
         let etree_element = etree
             .getattr("_Element")
             .expect("lxml.etree does not have _Element")
             .downcast::<PyType>()
             .expect("lxml.etree._Element is not a type")
-            .clone()
-            .unbind();
+            .clone();
         let etree_comment = etree
             .getattr("_Comment")
             .expect("lxml.etree does not have _Comment")
             .downcast::<PyType>()
             .expect("lxml.etree._Comment is not a type")
-            .clone()
-            .unbind();
+            .clone();
 
-        Self {
-            buf: Vec::with_capacity(DEFAULT_BUFFER_SIZE),
+        let write = match output {
+            Some(output) => Some(output.getattr(intern!(py, "write"))?),
+            None => None,
+        };
+
+        Ok(Self {
+            buf: Vec::with_capacity(MEM_BUFFER_SIZE),
             pos: 0,
             line_length,
+            write,
 
             etree_element,
             etree_comment,
-        }
+        })
     }
 
     fn feed_tree(mut self, tree: &Bound<PyAny>, siblings: bool) -> PyResult<Self> {
         let py = tree.py();
 
-        if !tree.is_instance(&self.etree_element.bind(py))? {
+        if !tree.is_instance(&self.etree_element)? {
             Err(PyTypeError::new_err(format!(
                 "Unserializable object type {}, expected lxml.etree._Element",
                 tree.get_type()
@@ -123,7 +133,7 @@ impl Serializer {
             drop(kwargs);
 
             for i in preceding_siblings.iter().rev() {
-                if !i.is_instance(&self.etree_comment.bind(py))? {
+                if !i.is_instance(&self.etree_comment)? {
                     Err(PyValueError::new_err(
                         "Non-comment before main tree, try 'siblings=False'",
                     ))?
@@ -131,7 +141,7 @@ impl Serializer {
 
                 check_has_no_tail(i)?;
                 self.eat_comment(i, 0)?;
-                self.emit_linebreak(0);
+                self.emit_linebreak(0)?;
             }
 
             check_has_no_tail(tree)?;
@@ -146,7 +156,7 @@ impl Serializer {
                 .expect("itersiblings did not return an iterable")
             {
                 let i = &i?;
-                if !i.is_instance(&self.etree_comment.bind(py))? {
+                if !i.is_instance(&self.etree_comment)? {
                     Err(PyValueError::new_err(
                         "Non-comment after main tree, try 'siblings=False'",
                     ))?
@@ -154,20 +164,25 @@ impl Serializer {
 
                 check_has_no_tail(i)?;
                 self.eat_comment(i, 0)?;
-                self.emit_linebreak(0);
+                self.emit_linebreak(0)?;
             }
         }
 
         Ok(self)
     }
 
-    fn finish(mut self) -> Vec<u8> {
-        self.emit_linebreak(0);
-        self.buf
+    fn finish(mut self) -> PyResult<Option<Vec<u8>>> {
+        self.emit_linebreak(0)?;
+        if let Some(write) = self.write {
+            write.call1((self.buf,))?;
+            Ok(None)
+        } else {
+            Ok(Some(self.buf))
+        }
     }
 }
 
-impl Serializer {
+impl<'py> Serializer<'py> {
     fn eat_comment(&mut self, element: &Bound<PyAny>, indent: usize) -> PyResult<()> {
         let py = element.py();
         let text = element
@@ -179,10 +194,10 @@ impl Serializer {
             .to_cow()
             .expect("comment text is not valid UTF-8");
 
-        self.emit_linebreak(indent);
-        self.emit_raw_string(b"<!--");
-        self.digest_multiline_text(&text, EscapeCharset::Comment);
-        self.emit_raw_string(b"-->");
+        self.emit_linebreak(indent)?;
+        self.emit_raw_string(b"<!--")?;
+        self.digest_multiline_text(&text, EscapeCharset::Comment)?;
+        self.emit_raw_string(b"-->")?;
         Ok(())
     }
 
@@ -193,7 +208,7 @@ impl Serializer {
         parent_nsmap: &HashMap<Cow<'_, str>, Cow<'_, str>>,
     ) -> PyResult<()> {
         let py = e.py();
-        assert!(e.is_instance(self.etree_element.bind(py)).unwrap_or(false));
+        assert!(e.is_instance(&self.etree_element).unwrap_or(false));
 
         let mut nsmap_alias2uri = e
             .getattr("nsmap")
@@ -214,10 +229,10 @@ impl Serializer {
             .map(|(k, v)| (v.to_string_lossy(), k.to_string_lossy()))
             .collect::<HashMap<Cow<'_, str>, Cow<'_, str>>>();
 
-        self.emit_raw_string(b"<");
+        self.emit_raw_string(b"<")?;
         let unresolved_tag = self.unresolve_namespace(e, &nsmap_uri2alias);
         let unresolved_tag = (unresolved_tag.0.as_ref(), unresolved_tag.1.as_str());
-        self.digest_namespaced_name(unresolved_tag);
+        self.digest_namespaced_name(unresolved_tag)?;
 
         let attribs = PyDict::new(py);
         attribs
@@ -243,28 +258,28 @@ impl Serializer {
                     .expect("attrib value is not a string");
                 let (ns, ln) = self.unresolve_namespace(attr, &nsmap_uri2alias);
                 if self.pos > self.line_length {
-                    self.emit_linebreak(indent + 2);
+                    self.emit_linebreak(indent + 2)?;
                 } else {
-                    self.emit_raw_string(b" ");
+                    self.emit_raw_string(b" ")?;
                 }
                 self.digest_attr_pair(
                     (ns.as_ref(), &ln),
                     &value.to_cow().expect("attrib value is not valid UTF-8") as &str,
-                );
+                )?;
             }
         }
 
         for (alias, uri) in nsmap_alias2uri.iter() {
             if !parent_nsmap.contains_key(&uri.to_cow()? as &str) {
                 if self.pos > self.line_length {
-                    self.emit_linebreak(indent + 2);
+                    self.emit_linebreak(indent + 2)?;
                 } else {
-                    self.emit_raw_string(b" ");
+                    self.emit_raw_string(b" ")?;
                 }
                 self.digest_attr_pair(
                     (Some(&Cow::Borrowed("xmlns")), &alias.to_cow()? as &str),
                     &uri.to_cow()? as &str,
-                );
+                )?;
             }
         }
 
@@ -279,14 +294,14 @@ impl Serializer {
                 .expect("attrib key/value is not a string 2-tuple");
             let (ns, key) = self.unresolve_namespace(&key, &nsmap_uri2alias);
             if force_break || self.pos > self.line_length {
-                self.emit_linebreak(indent + 2);
+                self.emit_linebreak(indent + 2)?;
             } else {
-                self.emit_raw_string(b" ");
+                self.emit_raw_string(b" ")?;
             }
             self.digest_attr_pair(
                 (ns.as_ref(), &key),
                 &value.to_cow().expect("attrib value is not valid UTF-8"),
-            );
+            )?;
 
             force_break = has_parent && ns.is_none() && key == "id";
         }
@@ -294,10 +309,10 @@ impl Serializer {
         let text = e.getattr(intern!(py, "text")).expect("element has no text");
         let has_children = e.len().expect("cannot get len() of element") > 0;
         if text.is_none() && !has_children && !ALWAYS_EXPANDED_TAGS.contains(&unresolved_tag) {
-            self.emit_raw_string(b"/>");
+            self.emit_raw_string(b"/>")?;
             return Ok(());
         }
-        self.emit_raw_string(b">");
+        self.emit_raw_string(b">")?;
 
         let mut trailing_text = if !text.is_none() {
             let text = text
@@ -306,26 +321,20 @@ impl Serializer {
             self.digest_multiline_text(
                 &text.to_cow().expect("element text is not valid UTF-8"),
                 EscapeCharset::Text,
-            );
+            )?;
             true
         } else {
             false
         };
         for child in e.try_iter().expect("cannot iterate over element") {
             if !trailing_text {
-                self.emit_linebreak(indent + 1);
+                self.emit_linebreak(indent + 1)?;
             }
 
             let child = child.expect("cannot iterate over element");
-            if child
-                .is_instance(self.etree_comment.bind(py))
-                .unwrap_or(false)
-            {
+            if child.is_instance(&self.etree_comment).unwrap_or(false) {
                 self.eat_comment(&child, indent + 1)?;
-            } else if child
-                .is_instance(self.etree_element.bind(py))
-                .unwrap_or(false)
-            {
+            } else if child.is_instance(&self.etree_element).unwrap_or(false) {
                 self.eat_element(&child, indent + 1, &nsmap_uri2alias)?;
             } else {
                 Err(PyTypeError::new_err(format!(
@@ -348,7 +357,7 @@ impl Serializer {
                 self.digest_multiline_text(
                     &tail.to_cow().expect("element tail is not valid UTF-8"),
                     EscapeCharset::Text,
-                );
+                )?;
                 true
             } else {
                 false
@@ -356,18 +365,18 @@ impl Serializer {
         }
 
         if has_children && !trailing_text {
-            self.emit_linebreak(indent);
+            self.emit_linebreak(indent)?;
         }
 
-        self.emit_raw_string(b"</");
-        self.digest_namespaced_name(unresolved_tag);
-        self.emit_raw_string(b">");
+        self.emit_raw_string(b"</")?;
+        self.digest_namespaced_name(unresolved_tag)?;
+        self.emit_raw_string(b">")?;
 
         py.check_signals()
     }
 }
 
-impl Serializer {
+impl<'py> Serializer<'py> {
     fn unresolve_namespace<'n>(
         &self,
         e: &Bound<PyAny>,
@@ -376,7 +385,7 @@ impl Serializer {
         let py = e.py();
         let tag = if let Ok(tag) = e.downcast::<PyString>() {
             tag.clone()
-        } else if e.is_instance(&self.etree_element.bind(py)).unwrap_or(false) {
+        } else if e.is_instance(&self.etree_element).unwrap_or(false) {
             e.getattr(intern!(py, "tag"))
                 .expect("element has no tag")
                 .downcast::<PyString>()
@@ -405,46 +414,81 @@ impl Serializer {
         }
     }
 
-    fn digest_string(&mut self, string: &str, charset: EscapeCharset) {
+    fn digest_string(&mut self, string: &str, charset: EscapeCharset) -> PyResult<()> {
         let string = escape(string, charset);
-        self.emit_raw_string(string.as_bytes());
+        self.emit_raw_string(string.as_bytes())
     }
 
-    fn digest_multiline_text(&mut self, text: &str, charset: EscapeCharset) {
+    fn digest_multiline_text(&mut self, text: &str, charset: EscapeCharset) -> PyResult<()> {
         for (i, line) in text.split('\n').enumerate() {
             if i > 0 {
-                self.emit_linebreak(0);
+                self.emit_linebreak(0)?;
             }
-            self.digest_string(line, charset);
+            self.digest_string(line, charset)?;
         }
+
+        Ok(())
     }
 
-    fn digest_namespaced_name(&mut self, name: (Option<&Cow<'_, str>>, &str)) {
+    fn digest_namespaced_name(&mut self, name: (Option<&Cow<'_, str>>, &str)) -> PyResult<()> {
         if let Some(ns) = name.0 {
-            self.emit_raw_string(ns.as_bytes());
-            self.emit_raw_string(b":");
+            self.emit_raw_string(ns.as_bytes())?;
+            self.emit_raw_string(b":")?;
         }
-        self.emit_raw_string(name.1.as_bytes());
+        self.emit_raw_string(name.1.as_bytes())
     }
 
-    fn digest_attr_pair(&mut self, key: (Option<&Cow<'_, str>>, &str), value: &str) {
-        self.digest_namespaced_name(key);
-        self.emit_raw_string(b"=\"");
-        self.digest_string(&value, EscapeCharset::Attribute);
-        self.emit_raw_string(b"\"");
+    fn digest_attr_pair(
+        &mut self,
+        key: (Option<&Cow<'_, str>>, &str),
+        value: &str,
+    ) -> PyResult<()> {
+        self.digest_namespaced_name(key)?;
+        self.emit_raw_string(b"=\"")?;
+        self.digest_string(&value, EscapeCharset::Attribute)?;
+        self.emit_raw_string(b"\"")
     }
 }
 
-impl Serializer {
-    fn emit_linebreak(&mut self, indent: usize) {
+impl<'py> Serializer<'py> {
+    fn emit_linebreak(&mut self, indent: usize) -> PyResult<()> {
+        if let Some(ref write) = self.write {
+            let needed_space = LINESEP.len() + INDENT_WIDTH * indent;
+            assert!(needed_space < MEM_BUFFER_SIZE);
+            if self.buf.len() + needed_space > MEM_BUFFER_SIZE {
+                write.call1((&self.buf,))?;
+                self.buf.clear();
+            }
+        }
+
         self.buf.extend(LINESEP);
         (0..INDENT_WIDTH * indent).for_each(|_| self.buf.push(INDENT_CHAR));
         self.pos = INDENT_WIDTH * indent;
+
+        Ok(())
     }
 
-    fn emit_raw_string(&mut self, string: &[u8]) {
-        self.buf.extend(string);
+    fn emit_raw_string(&mut self, string: &[u8]) -> PyResult<()> {
+        if let Some(ref write) = self.write {
+            let mut idx = 0;
+            loop {
+                let space = MEM_BUFFER_SIZE - self.buf.len();
+                self.buf.extend(string.iter().skip(idx).take(space));
+                idx += space;
+                if MEM_BUFFER_SIZE - self.buf.len() == 0 {
+                    write.call1((&self.buf,))?;
+                    self.buf.clear();
+                }
+                if idx >= string.len() {
+                    break;
+                }
+            }
+        } else {
+            self.buf.extend(string);
+        }
         self.pos += string.len();
+
+        Ok(())
     }
 }
 
